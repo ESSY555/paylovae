382d4b70b0bfccfc6e3d40b0edf007d5b477af15
ESSY555 <106964906+ESSY555@users.noreply.github.com>
2026-08-31 01:19:46 +0100

    new push

--- src/lib/links.ts
+++
@@ -1,4 +0,0 @@
-export function paymentLink(invoiceId: string) {
-  const origin = typeof window === "undefined" ? "https://payvolae.app" : window.location.origin;
-  return `${origin}/pay/${invoiceId}`;
-}
